d55fd58f4c66075c537986626326510dc64072aa
vivek0902 <viveksharma090297@gmail.com>
2025-02-26 12:00:00 +0640

    modified vite.config.js file to deploy on github

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "Deploy-a-Vite-React-Project-on-GitHub-Pages",
+  base: "Drag-and-drop-todoList-app",
   plugins: [react()],
 });
